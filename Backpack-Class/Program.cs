8612c9b49d229017ee7741d2233d734e09f22def
aelathy <112655204+aelathy@users.noreply.github.com>
2022-12-13 12:53:37 -0700

    Add files via upload

---
+++ Backpack-Class/Program.cs
@@ -0,0 +1,66 @@
+﻿// Backpack-Class Assignment
+#nullable disable
+Console.Clear();
+
+
+// Task 2
+Backpack bag1 = new Backpack("Blue", "Small");
+Backpack bag2 = new Backpack("Red", "Medium");
+Backpack bag3 = new Backpack("Green", "Large");
+
+
+//3
+bag1.openBag();
+bag1.putIn("Lunch");
+bag1.putIn("Jacket");
+bag1.closeBag();
+bag1.openBag();
+bag1.takeOut("Jacket");
+bag1.closeBag();
+
+
+
+
+class Backpack
+{
+    private List<string> item = new List<string>();
+    public string Color { get; set; }
+    public string Size { get; set; }
+    public List<string> Item { get; set; }
+    public bool Open { get; set; }
+
+    public Backpack(string color, string size)
+    {
+        this.Color = color;
+        this.Size = size;
+        this.Item = new List<string>();
+        Open = false;
+    }
+    public void openBag()
+    {
+        this.Open = true;
+    }
+
+    public void closeBag()
+    {
+        this.Open = false;
+    }
+
+    public string putIn(string item)
+    {
+        if (Open)
+        {
+            Item.Add(item);
+        }
+        return null;
+    }
+
+    public string takeOut(string item)
+    {
+        if (Open)
+        {
+            Item.Remove(item);
+        }
+        return null;
+    }
+}
